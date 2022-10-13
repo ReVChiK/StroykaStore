1aba220cf3ed0784e12cd44ad224b502e90d1cfa
ReVChik <Maks.kuzmin20@yandex.ru>
2022-10-13 18:58:10 +0300

    Apcent Stocks is completed

--- src/assets/js/functions.js
+++ src/assets/js/functions.js
@@ -9,6 +9,12 @@ import basket from '../img/Basket.png';
 import slide_1 from '../img/slide_1.png';
 import slide_2 from '../img/slide_2.png';
 
+import foarm from '../img/cards_socks/foam.png';
+import grid from '../img/cards_socks/grid.png';
+import metal_tile from '../img/cards_socks/metal_tile.png';
+import paint from '../img/cards_socks/paint.png';
+
+
 // Favicon
 function Add_Favicon(){
     var link = document.querySelector("link[rel~='shortcut']");
@@ -17,11 +23,24 @@ function Add_Favicon(){
 }
 // Favicon
 
+// Cards-Images
+function Cards_Images(){
+    var paint_img = document.querySelector('.paint-img');
+    var foarm_img = document.querySelector('.foam-img');
+    var grid_img = document.querySelector('.grid-img');
+    var metal_tile_img = document.querySelector('.metal_tile-img');
+
+    paint_img.src = paint;
+    foarm_img.src = foarm;
+    grid_img.src = grid;
+    metal_tile_img.src = metal_tile;
+}
+// Cards-Images
+
 // Slider Images
 function set_background_slider_img(){
     var slider_container = document.querySelector('.slider-container');
 
-    let imgs_array = [slide_1, slide_2];
     let count = 1;
 
     let slider_btn_left = document.querySelector('.slider-btn_left');
@@ -29,14 +48,27 @@ function set_background_slider_img(){
     let slider_btn_right = document.querySelector('.slider-btn_right');
 
     slider_btn_left.addEventListener('click', function(){
-        count =- 1;
-        if(count < 1){
-            slider_container.style.backgroundImage = `url(${imgs_array[slide_2]})`
+        count -= 1;
+        if(count % 2 == 0){
+            slider_container.style.backgroundImage = `url(${slide_2})`;
+        }else{
+            slider_container.style.backgroundImage = `url(${slide_1})`;
         }
-        slider_container.style.backgroundImage = `url(${slide_1})`;
     })
 
-    slider_container.style.backgroundImage = `url(${slide_1})`;
+    slider_btn_right.addEventListener('click', function(){
+        count += 1
+        if(count % 2 == 1){
+            slider_container.style.backgroundImage = `url(${slide_1})`;
+        }else{
+            slider_container.style.backgroundImage = `url(${slide_2})`;
+        }
+    })
+
+    if(count > 0 && count < 2){
+        slider_container.style.backgroundImage = `url(${slide_1})`;
+    }
+
 }
 // Slider Images
 
@@ -82,6 +114,7 @@ function Webpack_Main(){
     Set_search_Icon();
     Set_Img_Panel();
     set_background_slider_img();
+    Cards_Images();
 }
 
 export default Webpack_Main;
