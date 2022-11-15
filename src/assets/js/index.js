5bfb8f3350aad2841fc7c1e0dc3976fae75dfab5
ReVChik <Maks.kuzmin20@yandex.ru>
2022-11-15 18:31:01 +0300

    Page Brands is finished

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,9 +1,14 @@
 import '../sass/style.sass';
 import Webpack_Main from './functions.js';
-import './components/components.js';
+import Component from './components/components.js';
 
 import Main from './events/events';
 
-Webpack_Main();
-Main();
+function Index(){
+    Webpack_Main();
+    Main();
+    Component();
+}
+
+window.addEventListener('DOMContentLoaded', Index);
 
